618720776b589678289f7fd3678c3725cfec3499
mohammad <mohammadadel8727@gmail.com>
2022-02-26 21:01:58 +0200

    added book design and style events

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 // -------------------- global variables --------------------
+// main object holds every data
+class Book {
+    constructor(title, author, number, img, category, id){
+        this.title = title;
+        this.author = author;
+        this.number = number;
+        this.img = img;
+        this.category = category;
+        this.id = id;
+    }
+}
+
 // object with all methods performed on page
 const buttonAction = {
     // drop down menu
@@ -6,6 +18,8 @@ const buttonAction = {
     // main form
     newCancel: hideForm,
     addBook: saveBook,
+    showMore: displayBookForm,
+    cancel: hideBookForm,
 }
 
 // drop down menu
@@ -20,7 +34,7 @@ const mainForm = document.querySelector('.form-container');
 const mainSections = document.querySelectorAll('.category');
 
 // -------------------- main program --------------------
-// a lot of crazy stuff will go here almost all program will go here
+// a lot of crazy stuff will go here almost all program will run here
 window.addEventListener('click', (e)=>{
     // drop down menu
     (e.target.dataset.toggle)? performMenu('toggle') : performMenu('remove');
@@ -37,6 +51,11 @@ window.addEventListener('click', (e)=>{
 })
 
 // -------------------- helper functions --------------------
+// dealing with css class
+function operateElement(ele, operation, style) {
+    ele.classList[operation](style)
+}
+
 // drop down menu
 function performMenu(action) {
     menuIcon.classList[action]('active-menu-icon');
@@ -46,15 +65,20 @@ function performMenu(action) {
 // category filter
 function filterCategory(category) {
     if (category === 'all') {
+        // remove style from all sections
         mainSections.forEach(section=> section.classList.remove('category-hide'));
+        // remove style from other links
         menuItems.querySelectorAll('li').forEach(item=> item.classList.remove('active-item'));
+        // add active style to all button
         menuItems.querySelector('[data-category="all"]').classList.add('active-item');
     } 
     else {
+        // adding active class to selected section
         mainSections.forEach(section=>{
-            section.dataset.category !== category? section.classList.add('category-hide'):
+            section.dataset.section !== category? section.classList.add('category-hide'):
             section.classList.remove('category-hide');
         });
+        // adding active class to clicked list item
         menuItems.querySelectorAll('li').forEach(item=>{
             item.dataset.category !== category? item.classList.remove('active-item'):
             item.classList.add('active-item');
@@ -75,4 +99,13 @@ function hideForm() {
 
 function saveBook() {
     console.log('book saved');
+}
+
+// book
+function displayBookForm(btn) {
+    btn.closest('.book').classList.add('edit-book');
+}
+
+function hideBookForm(btn) {
+    btn.closest('.book').classList.remove('edit-book');
 }
