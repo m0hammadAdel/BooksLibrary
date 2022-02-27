933689b02e91878e0c63b02052aea062a9fdf800
mohammad <mohammadadel8727@gmail.com>
2022-02-27 22:38:12 +0200

    Project finished and added all functinoality

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // -------------------- global variables --------------------
 // main object holds every data
 class Book {
-    constructor(title, author, number, img, category, id){
+    constructor(title, author, number, img="undefined", category){
         this.title = title;
         this.author = author;
         this.number = number;
         this.img = img;
         this.category = category;
-        this.id = id;
+    };
+    showBookInfo = ()=> {
+        console.log(this.author, this.title, this.number, this.img, this. category);
     }
 }
 
@@ -18,8 +20,12 @@ const buttonAction = {
     // main form
     newCancel: hideForm,
     addBook: saveBook,
+    newClear: clearForm,
+    // book
     showMore: displayBookForm,
     cancel: hideBookForm,
+    save: editBook,
+    delete: removeBook
 }
 
 // drop down menu
@@ -48,14 +54,14 @@ window.addEventListener('click', (e)=>{
         e.preventDefault();
         buttonAction[e.target.dataset.id](e.target);
     }
+    // change a certain book category
+    else if (e.target.dataset.book) {
+        e.preventDefault();
+        changeCategoryBook(e.target);
+    }
 })
 
 // -------------------- helper functions --------------------
-// dealing with css class
-function operateElement(ele, operation, style) {
-    ele.classList[operation](style)
-}
-
 // drop down menu
 function performMenu(action) {
     menuIcon.classList[action]('active-menu-icon');
@@ -97,15 +103,149 @@ function hideForm() {
     mainForm.classList.remove('display-form');
 }
 
+function clearForm() {
+    mainForm.querySelector('#new-title').value = " ";
+    mainForm.querySelector('#new-author').value = " ";
+    mainForm.querySelector('#new-pages').value = null;
+    mainForm.querySelector('#new-url').value = " ";
+}
+
 function saveBook() {
-    console.log('book saved');
+    let newCategory;
+    mainForm.querySelectorAll('.radio-input').forEach(input=>{
+        if(input.checked) newCategory = input.value;
+    });
+
+    const newBook = new Book(
+        mainForm.querySelector('#new-title').value,
+        mainForm.querySelector('#new-author').value,
+        mainForm.querySelector('#new-pages').value,
+        mainForm.querySelector('#new-url').value,
+        newCategory
+    );
+
+    addBookToPage(newBook);
+    clearForm();
+    hideForm();
 }
 
 // book
 function displayBookForm(btn) {
+    btn.closest('.books-container').querySelectorAll('.book').forEach(book=> book.classList.remove('edit-book'));
     btn.closest('.book').classList.add('edit-book');
+    const book = btn.closest('.book');
+    const bookForm = book.querySelector('.book-form');
+    bookForm.querySelector('.edit-title').value = book.querySelector('.title').textContent;
+    bookForm.querySelector('.edit-author').value = book.querySelector('.author').textContent;
+    bookForm.querySelector('.edit-pages').value = book.querySelector('.number').textContent;
+    bookForm.querySelector('.edit-url').value = book.querySelector('img').src;
+    const category = book.dataset.bookcat;
+    bookForm.querySelectorAll('.category-btn').forEach(btn=>{
+        btn.classList.remove('active-category-btn');
+        if (btn.dataset.book === category) btn.classList.add('active-category-btn');
+    })
 }
 
 function hideBookForm(btn) {
     btn.closest('.book').classList.remove('edit-book');
+}
+
+function changeCategoryBook(btn) {
+    const container = btn.closest('.category-btns');
+    container.querySelectorAll('.category-btn').forEach(catBtn=>{
+        catBtn.classList.remove('active-category-btn');
+    });
+    btn.classList.add('active-category-btn');
+}
+
+function editBook(btn) {
+    const bookForm = btn.closest('.book-form');
+    const book = btn.closest('.book');
+    const title = bookForm.querySelector('.edit-title').value;
+    const author = bookForm.querySelector('.edit-author').value;
+    const number = bookForm.querySelector('.edit-pages').value;
+    const url = bookForm.querySelector('.edit-url').value;
+    let newCategory;
+    bookForm.querySelectorAll('.category-btn').forEach(btn=>{
+        if (btn.classList.contains('active-category-btn')) newCategory = btn.dataset.book;
+    });
+    if (newCategory === book.dataset.bookcat) {
+        book.querySelector('.title').textContent = title;
+        book.querySelector('.author').textContent = author;
+        book.querySelector('.number').textContent = number;
+        book.querySelector('.front img').src = url;
+        book.querySelector('.back img').src = url;
+        hideBookForm(btn);
+    }
+    else {
+        const newBook = new Book(title, author, number, url, newCategory);
+        addBookToPage(newBook);
+        book.remove();
+    }
+}
+
+function removeBook(btn){
+    const targetBook = btn.closest('.book');
+    const currentSection = targetBook.closest('section');
+    targetBook.remove();
+    if (!currentSection.querySelector('.book')) currentSection.querySelector('.books-container').innerHTML = `<p>this shelf is empty</p>`;
+}
+
+function addBookToPage(book) {
+    const newBook = document.createDocumentFragment();
+    newBook.innerHTML = `
+    <article class="book" data-bookcat="${book.category}">
+        <div class="front">
+            <img src="${book.img}" alt="">
+            <div class="info">
+                <h3 class="title">${book.title}</h3>
+                <p class="author">${book.author}</p>
+                <p class="number">${book.number}</p>
+            </div>
+            <div class="show-more">
+                <button title="edit" data-id="showMore">
+                    <i class="fa-solid fa-angles-right" data-id="showMore"></i>
+                </button>
+                <p>edit</p>
+            </div>
+        </div>
+        <div class="back">
+            <img src="${book.img}" alt="">
+            <form class="book-form">
+                <div class="field">
+                    <label>title</label>
+                    <input type="text" class="edit-title">
+                </div>
+                <div class="field">
+                    <label>author</label>
+                    <input type="text" class="edit-author">
+                </div>
+                <div class="field">
+                    <label>pages</label>
+                    <input type="number" class="edit-pages">
+                </div>
+                <div class="field">
+                    <label>cover url</label>
+                    <input type="text" class="edit-url">
+                </div>
+                <div class="category-btns">
+                    <button class="category-btn" data-book="reading">reading</button>
+                    <button class="category-btn" data-book="wishList">wish list</button>
+                    <button class="category-btn" data-book="done">done</button>
+                </div>
+                <div class="new-buttons">
+                    <button class="new-btn cancel" data-id="cancel">cancel</button>
+                    <button class="new-btn delete" data-id="delete">delete</button>
+                    <button class="new-btn" data-id="save">save</button>
+                </div>
+            </form>
+        </div>
+    </article>`;
+
+    mainSections.forEach(section=>{
+        if (section.dataset.section === book.category) {
+            if(!section.querySelector('.book')) section.querySelector('.books-container').innerHTML = '';
+            section.querySelector('.books-container').innerHTML += newBook.innerHTML;
+        }
+    })
 }
